3d6410ea2bc83d831ec29dad2815e802ce56fbb8
syropian <collinpatrickhenderson@gmail.com>
2018-10-26 19:58:32 -0400

    Store token in ls

--- utils/auth.js
+++ utils/auth.js
@@ -1,15 +1,20 @@
 import Cookie from 'js-cookie'
+import ls from 'local-storage'
 
 export const setToken = token => {
   if (process.server) return
   Cookie.set('access_token', token)
+  ls('access_token', token)
 }
 
 export const getToken = req => {
   if (!req) {
     return Cookie.get('access_token')
+      ? Cookie.get('access_token')
+      : ls('access_token')
   } else {
-    return req.headers.hasOwnProperty('cookie') && req.headers.cookie.indexOf('access_token') > -1
+    return req.headers.hasOwnProperty('cookie') &&
+      req.headers.cookie.indexOf('access_token') > -1
       ? req.headers.cookie
           .split(';')
           .find(c => c.trim().startsWith('access_token='))
