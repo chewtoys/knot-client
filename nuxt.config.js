766f1e80d4bac18734ba0825d656a568e5b9d9bb
syropian <collinpatrickhenderson@gmail.com>
2019-05-15 18:59:23 -0400

    Adds bugsnag plugin

--- nuxt.config.js
+++ nuxt.config.js
@@ -70,6 +70,7 @@ module.exports = {
   ** Plugins to load before mounting the App
   */
   plugins: [
+    { src: '~/plugins/bugsnag.js', ssr: false },
     { src: '~/plugins/event-bus.js', ssr: true },
     { src: '~/plugins/localStorage.js', ssr: false },
     { src: '~/plugins/lazysizes.js', ssr: false },
